49c85c659fcbd9f7a29b74ec459ec0f5c4bea904
Helder <heldrida@gmail.com>
2014-09-19 01:39:52 +0100

    completed step 4: by doing a synchronized animation. moving a surface from the top of the context to the bottom

--- app/src/main.js
+++ app/src/main.js
@@ -4,42 +4,40 @@ define(function(require, exports, module) {
 
     var Engine = require('famous/core/Engine');
     var Surface = require('famous/core/Surface');
-    var Transform = require('famous/core/Transform');
     var Modifier = require('famous/core/Modifier');
-    var Easing = require('famous/transitions/Easing');
+    var Transform = require('famous/core/Transform');
     var Transitionable = require('famous/transitions/Transitionable');
 
     var mainContext = Engine.createContext();
 
-    var transitionable = new Transitionable(0);
+    // creates transitionable with array value
+    // storing the align and origin states
+    var alignOrigin = new Transitionable([0, 0]);
 
     var surface = new Surface({
+        size: [undefined, 100],
         properties: {
             backgroundColor: '#FA5C4F'
         }
     });
 
     var modifier = new Modifier({
-        size: [200, 200],
-        origin: [0.5, 0.5],
-        align: [0.5, 0.5],
-        transform: function () {
-            // cache value of transitionable.get()
-            var scale = transitionable.get();
-            return Transform.scale(scale, scale, 1);
+        align: function () {
+            // transition align from [0, 0] and [0, 1]
+            return alignOrigin.get();
         },
-        opacity: function () {
-            return transitionable.get();
+        origin: function () {
+            // transition origin from [0, 0] and [0, 1]
+            return alignOrigin.get();
         }
     });
 
     mainContext.add(modifier).add(surface);
 
-    // transitioning he transitionable from 0 to 1
-    // with the transition definition
-    transitionable.set(1, {
-        duration: 2000,
-        curve: Easing.outBack
+    // transitioning the alignOrigin state
+    alignOrigin.set([0, 1], {
+        duration: 1000,
+        curve: 'easeInOut'
     });
 
 });
